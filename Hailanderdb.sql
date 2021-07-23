6fdcc4c871422a31ee4d336855d3d116c1b918cc
hailanderhb <hailanderhb@gmail.com>
2021-07-22 22:12:05 -0300

    Adicionando querys em sql utilizada para estudos

---
+++ Hailanderdb.sql
@@ -0,0 +1,104 @@
+CREATE DATABASE [Hailanderdb]
+
+USE [Hailanderdb]
+GO
+
+CREATE TABLE [Student](
+    [Id] UNIQUEIDENTIFIER NOT NULL, --GUID > INT
+    [Name] NVARCHAR(120) NOT NULL,
+    [Email] NVARCHAR (180) NOT NULL,
+    [Document] NVARCHAR(20) NULL,
+    [Phone] NVARCHAR(20) NULL,
+    [Birthdate] DATETIME NULL,
+    [CreateDate] DATETIME NOT NULL DEFAULT(GETDATE()),
+    CONSTRAINT [PK_Student] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Author](
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR(120) NOT NULL,
+    [Title] NVARCHAR(80) NULL,
+    [Image] NVARCHAR(1024) NOT NULL,
+    [Bio] NVARCHAR(2000) NULL,
+    [Url] NVARCHAR (450) NULL,
+    [Email] NVARCHAR (160) NOT NULL,
+    [Type] TINYINT NOT NULL,
+    CONSTRAINT [PK_Author] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [Career](
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(80) NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR (450) NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [Active] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    CONSTRAINT [PK_Career] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Category](
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR (1024) NULL,
+    [Order] INT NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Featured] BIT NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Course](
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Tag] NVARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(160) NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR (1024) NULL,
+    [Level] TINYINT NOT NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [CreateDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NOT NULL,
+    [Active] BIT NOT NULL,
+    [Free] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [AuthorId] UNIQUEIDENTIFIER NULL,
+    [CategoryId] UNIQUEIDENTIFIER NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Course_Author_AuthorId] FOREIGN KEY ([AuthorId]) REFERENCES [Author] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_Course_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE NO ACTION
+);
+GO
+
+
+CREATE TABLE [CareerItem](
+    [CourseId] UNIQUEIDENTIFIER NOT NULL,
+    [CareerId] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NULL,
+    [Description] TEXT NOT NULL,
+    [Order] TINYINT NOT NULL,
+    CONSTRAINT [PK_CareerItem] PRIMARY KEY ([CourseId], [CareerId]),
+    CONSTRAINT [FK_CareerItem_Career_CareerId] FOREIGN KEY ([CareerId]) REFERENCES [Career] ([Id]),
+    CONSTRAINT [FK_CareerItem_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id])
+);
+GO
+
+
+CREATE TABLE [StudentCourse](
+    [CourseId] UNIQUEIDENTIFIER NOT NULL,
+    [StudentId] UNIQUEIDENTIFIER NOT NULL,
+    [Progress] TINYINT NOT NULL,
+    [Favorite] BIT NOT NULL,
+    [StartDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NULL,
+    CONSTRAINT [PK_StudentCourse] PRIMARY KEY ([CourseId], [StudentId]),
+    CONSTRAINT [FK_StudentCourse_Course_CourseID] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id]),
+    CONSTRAINT [FK_StudentCourse_Student_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Student] ([Id])
+);
+GO
